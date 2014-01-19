81649ed6ea68da04f0c5ca010be0a3d8d9e2cf52
Joseph Wu <kaysoky88@gmail.com>
2014-01-19 12:43:26 -0800

    Finished up shell (part 1). Manually tested, and it seemed to work.

--- project1/shell/fsh.c
+++ project1/shell/fsh.c
@@ -2,6 +2,10 @@
 #include <readline/readline.h>
 #include <string.h>
 #include <assert.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <errno.h>
 #include "queue.h"
 
@@ -21,7 +25,7 @@ const char* BUILT_IN_COMMAND_CD = "cd";
 // When the "cd" command is used without arguments
 const char* DEFAULT_CD_ENVIRO_VAR = "HOME";
 
-// This is either the default exit code (0) 
+// This is either the default exit code (0)
 // or the exit code of the last executed program
 int exit_code = 0;
 
@@ -39,12 +43,12 @@ static int tokenize(char *line, char ***returned_tokens) {
     // Note: no memory is allocated by strtok()
     char *token = strtok(line, STRING_TOKEN_DELIMITERS);
     queue_append(token_queue, (queue_element*) token);
-    
+
     // Continue to tokenize until there are no more tokens
     while ((token = strtok(NULL, STRING_TOKEN_DELIMITERS))) {
         queue_append(token_queue, (queue_element*) token);
     }
-    
+
     // Initialize the return values
     size_t num_tokens = queue_size(token_queue);
     char **tokens = (char **) malloc((num_tokens + 1) * sizeof(char *));
@@ -52,21 +56,21 @@ static int tokenize(char *line, char ***returned_tokens) {
         // Uh oh, malloc fail!
         exit(EXIT_FAILURE);
     }
-    
+
     // Convert from queue to array
     for (int i = 0; i < num_tokens; i++) {
         bool res = queue_remove(token_queue, (queue_element**) &tokens[i]);
-        
+
         // The removal must work
         assert(res);
     }
-    
+
     // All tokens must have been transferred from queue to array
     assert(queue_is_empty(token_queue));
-    
+
     // The token array must be NULL-terminated
     tokens[num_tokens] = (char *) NULL;
-    
+
     // Return the tokens and number of tokens
     queue_destroy(token_queue);
     *returned_tokens = tokens;
@@ -77,8 +81,8 @@ static int tokenize(char *line, char ***returned_tokens) {
 // Using either the second token or the user's HOME variable
 // If neither are found, nothing happens
 static void change_directory(const int num_tokens, const char **tokens) {
-    char *directory;
-    
+    const char *directory;
+
     // Is there an additional argument?
     if (num_tokens > 1) {
         directory = tokens[1];
@@ -86,27 +90,63 @@ static void change_directory(const int num_tokens, const char **tokens) {
         // Grab the directory from the user's HOME variable
         directory = getenv(DEFAULT_CD_ENVIRO_VAR);
     }
-    
+
     // Nothing to do here
     // This happens if no argument is supplied
     // and the environmental variable is not defined
     if (directory == NULL) {
         return;
     }
-    
+
     // Make a system call to change the working directory
     int res = chdir(directory);
-    
+
     // Let the user know if the system call fails
     if (res != 0) {
-        perror();
+        perror("chdir");
     }
 }
 
 // Treats the first token as a path to an executable
 // And executes it, using the subsequent tokens as arguments
 static void execute_program(const int num_tokens, const char **tokens) {
-    //TODO
+    // Fork the process
+    int status;
+    pid_t pid = fork();
+
+    // Did the fork succeed?
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+        // Child process
+        status = execvp(tokens[0], (char * const*) &tokens[0]);
+        if (status == -1) {
+            perror(tokens[0]);
+            exit(EXIT_FAILURE);
+        }
+
+    } else {
+        // Parent process
+        pid_t terminated = wait(&status);
+
+        // Did the wait succeed?
+        if (terminated == -1) {
+            perror("wait");
+            exit(EXIT_FAILURE);
+        }
+
+        // The process IDs should match
+        assert(pid == terminated);
+
+        // Did the child terminated normally?
+        if (WIFEXITED(status)) {
+            // Save the exit code
+            exit_code = WEXITSTATUS(status);
+        }
+    }
 }
 
 int main() {
@@ -116,7 +156,7 @@ int main() {
         char *line = readline(SHELL_PROMPT);
         char **tokens;
         int num_tokens = tokenize(line, &tokens);
-        
+
         // Is there any input?
         if (num_tokens == 0) {
             // Then clean up and loop
@@ -124,7 +164,7 @@ int main() {
             free(line);
             continue;
         }
-        
+
         // Interpret the input line by looking at the first token
         if (strcmp(tokens[0], BUILT_IN_COMMAND_EXIT) == 0) {
             // Is there an additional argument?
@@ -136,17 +176,17 @@ int main() {
                 long val;
 
                 // To distinguish success/failure after the call
-                errno = 0;    
+                errno = 0;
                 val = strtol(tokens[1], &endptr, 10);
 
                 // Is there integer overflow?
                 // Or did some other unexpected error occur?
                 if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
                         || (errno != 0 && val == 0)) {
-                    perror();
+                    perror("strtol");
                     val = exit_code;
                 }
-                
+
                 // Was a number given at all?
                 if (endptr == tokens[1]) {
                     val = exit_code;
@@ -156,26 +196,26 @@ int main() {
                 // Or set the value to be the default exit code
                 exit_code = val;
             }
-            
+
             // Exit with the appropriate code
             free(tokens);
             free(line);
             exit(exit_code);
         }
-        
+
         // Check for the "cd" command
         if (strcmp(tokens[0], BUILT_IN_COMMAND_CD) == 0) {
-            change_directory(num_tokens, tokens);
-            
+            change_directory(num_tokens, (const char **) tokens);
+
             // Clean up before looping
             free(tokens);
             free(line);
             continue;
         }
-        
+
         // Treat the first argument as a path to an executable
-        execute_program(num_tokens, tokens);
-        
+        execute_program(num_tokens, (const char **) tokens);
+
         // Clean up before looping
         free(tokens);
         free(line);
