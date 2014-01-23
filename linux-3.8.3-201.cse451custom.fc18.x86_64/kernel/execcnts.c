67452977e72f9c180432173e421c3a0daac29af6
Joseph Wu <kaysoky88@gmail.com>
2014-01-23 14:03:54 -0800

    Based on what was demonstrated in section...

--- linux-3.8.3-201.cse451custom.fc18.x86_64/kernel/execcnts.c
+++ linux-3.8.3-201.cse451custom.fc18.x86_64/kernel/execcnts.c
@@ -1,6 +1,8 @@
+#include <linux/kernel.h>
+#include <linux/types.h>
 #include <linux/syscalls.h>
 
-SYSCALL_DEFINE1(execcnts, unsigned int, pid)
-{
-  printk("Hello world: execcnts");
+SYSCALL_DEFINE1(execcnts, pid_t, pid) {
+  printk("Hello world: execcnts\n");
+  return 0;
 }
