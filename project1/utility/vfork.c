a3c69301867dfbdbb5ba8527bfb47a444bef01ae
Joseph Wu <kaysoky88@gmail.com>
2014-01-31 12:19:08 -0800

    More incorrectness...

--- project1/utility/vfork.c
+++ project1/utility/vfork.c
@@ -2,13 +2,6 @@
 #include <sys/types.h>
 
 int main() {
-    pid_t pid = syscall(58);
-    if (pid == 0) {
-        char *test[2];
-        test[0] = "fork";
-        test[1] = "\0";
-        syscall(59, 1, test);
-    }
-    
-    return 0;
+    syscall(58);
+    exit(0);
 }
